8ca6dbf4ce85ac62b9a76a3b8f79744b7e7998af
Alex Yu <alyu@softlayer.com>
2016-02-14 23:10:59 -0600

    add options observer

--- addon/components/sl-chart.js
+++ addon/components/sl-chart.js
@@ -128,13 +128,9 @@ export default Ember.Component.extend({
     setupChart: Ember.on(
         'didInsertElement',
         function() {
-            const chartDiv = this.$( 'div.chart' );
-
             this.setHeight();
             this.setWidth();
-
-            chartDiv.highcharts( this.get( 'highchartsOptions' ) );
-            this.set( 'chart', chartDiv.highcharts() );
+            this.updateOptions();
             this.updateData();
         }
     ),
@@ -178,6 +174,14 @@ export default Ember.Component.extend({
         }
     ),
 
+    updateOptions: Ember.observer(
+        'options',
+        function() {
+            const chartDiv = this.$( 'div.chart' );
+            this.set( 'chart', chartDiv.highcharts( this.highchartsOptions() ) );
+        }
+    ),
+
     // -------------------------------------------------------------------------
     // Methods
 
@@ -187,75 +191,73 @@ export default Ember.Component.extend({
      * @function
      * @returns {Object}
      */
-    highchartsOptions: Ember.computed(
-        function() {
-            const chartStyle = {
-                fontFamily: [
-                    '"Benton Sans"',
-                    '"Helvetica Neue"',
-                    'Helvetica',
-                    'Arial',
-                    'sans-serif'
-                ].join( ', ' ),
-                fontSize: '13px'
-            };
+    highchartsOptions() {
+        const chartStyle = {
+            fontFamily: [
+                '"Benton Sans"',
+                '"Helvetica Neue"',
+                'Helvetica',
+                'Arial',
+                'sans-serif'
+            ].join( ', ' ),
+            fontSize: '13px'
+        };
 
-            const options = Ember.$.extend( true, {
-                title: '',
-                chart: {
-                    animation: false,
-                    backgroundColor: 'rgba(255, 255, 255, 0)',
-                    style: chartStyle
+        const options = Ember.$.extend( true, {
+            title: '',
+            chart: {
+                animation: false,
+                backgroundColor: 'rgba(255, 255, 255, 0)',
+                style: chartStyle
+            },
+            colors: [
+                '#298fce',
+                '#94302e',
+                '#00a14b',
+                '#f29c1e',
+                '#fadb00',
+                '#34495d'
+            ],
+            credits: {
+                enabled: false
+            },
+            legend: {
+                itemStyle: chartStyle
+            },
+            plotOptions: {
+                bar: {
+                    borderColor: 'transparent'
                 },
-                colors: [
-                    '#298fce',
-                    '#94302e',
-                    '#00a14b',
-                    '#f29c1e',
-                    '#fadb00',
-                    '#34495d'
-                ],
-                credits: {
-                    enabled: false
-                },
-                legend: {
-                    itemStyle: chartStyle
-                },
-                plotOptions: {
-                    bar: {
-                        borderColor: 'transparent'
-                    },
-                    series: {
-                        animation: false
-                    }
-                },
-                tooltip: {
-                    animation: false,
-                    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                    borderWidth: 0,
-                    shadow: false,
-                    style: {
-                        color: '#fff'
-                    }
-                },
-                xAxis: {
-                    labels: {
-                        style: chartStyle
-                    }
-                },
-                yAxis: {
-                    labels: {
-                        style: chartStyle
-                    }
+                series: {
+                    animation: false
                 }
-            }, this.get( 'options' ) || {} );
+            },
+            tooltip: {
+                animation: false,
+                backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                borderWidth: 0,
+                shadow: false,
+                style: {
+                    color: '#fff'
+                }
+            },
+            xAxis: {
+                labels: {
+                    style: chartStyle
+                }
+            },
+            yAxis: {
+                labels: {
+                    style: chartStyle
+                }
+            }
+        }, this.get( 'options' ) || {} );
 
-            // Title property in options must be kept null in order to
-            // suppress its default behavior for our specific usage
-            options.title = null;
+        // Title property in options must be kept null in order to
+        // suppress its default behavior for our specific usage
+        options.title = null;
 
-            return options;
-        }
-    )
+        return options;
+    }
 
 });
